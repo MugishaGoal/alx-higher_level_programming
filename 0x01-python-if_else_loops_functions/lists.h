ce5332f0a814d18e433087c2f1dcb96303dab923
Mugisha Goal <degaullemugisha@gmail.com>
2023-08-10 22:33:07 +0000

    Header file

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,23 +1,24 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib.h>
-
 /**
- * struct listint_s - singly linked lists
+ * struct listint_s - singly linked list
  * @n: integer
- * @next_node: Points to the next node
+ * @next: points to the next node
  *
- * Description: A singly linked list
+ * Description: singly linked list node structure
+ *
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next_node;
-}listint_z;
-size_t print_listint(const listint_z *head);
-listint_z *add_node(listint_z **head, const int n);
-void free listint(listint_z *head);
-listint_z *add_node(listint_z *head int x);
+    int n;
+    struct listint_s *next;
+} listint_t;
 
-#endif /* LIST_H */
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
+listint_t *insert_node(listint_t **head, int number);
+
+#endif /* LISTS_H */
