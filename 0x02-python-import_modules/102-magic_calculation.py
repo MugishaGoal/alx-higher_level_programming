135b60cf5ebb16120597db4abfd499561ade1229
Mugisha Goal <degaullemugisha@gmail.com>
2023-08-19 05:14:34 -0700

    def magic_calculation(a, b)

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,10 +4,10 @@ def magic_calculation(a, b):
     add = __import__('magic_calculation_102').add
     sub = __import__('magic_calculation_102').sub
 
-    if x < y:
-        z = add(x, y)
-        for i in range(4, 6):
-            z = add(z, i)
+    if a < b:
+        z = add(a, b)
+        for x in range(4, 6):
+            z = add(z, x)
         return z
     else:
-        return sub(x, y)
+        return sub(a, b)
