4539dd7b35ceeccc10ef386aa0651e00b09d4b02
Mugisha Goal <degaullemugisha@gmail.com>
2023-09-06 11:17:12 -0700

    A program that solves the N queens problem.

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -7,18 +7,16 @@ def is_safe(board, row, col):
     for i in range(row):
         if board[i][col] == 1:
             return False
-    
     """Check upper left diagonal"""
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-    
     """Check upper right diagonal"""
     for i, j in zip(range(row, -1, -1), range(col, len(board))):
         if board[i][j] == 1:
             return False
-    
     return True
+
 
 def solve_nqueens(board, row):
     n = len(board)
@@ -36,23 +34,22 @@ def solve_nqueens(board, row):
                 solve_nqueens(board, row + 1)
                 board[row][col] = 0
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    
     try:
         N = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-    
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
-    
+
     """Initialize the chessboard"""
     chessboard = [[0 for _ in range(N)] for _ in range(N)]
-    
+
     """Solve the N queens problem"""
     solve_nqueens(chessboard, 0)
