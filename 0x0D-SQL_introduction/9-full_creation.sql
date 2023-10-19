84d814a1b060c6bd2fece1ae8b589781551c93db
Mugisha Goal <degaullemugisha@gmail.com>
2023-10-19 12:13:19 -0700

    script that creates a table second_table in the database hbtn_0c_0

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- A script that creates a table second_table in the database hbtn_0c_0
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+
+-- Insert records into the second_table
+INSERT INTO second_table (id, name, score)
+VALUES (1, 'John' 10), (2, 'Alex', 3), (3, 'BOb', 14), (4, 'George', 8);
