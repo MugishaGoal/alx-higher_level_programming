1f59c8c4cfcc4275539d13970c3450b3e1843721
Mugisha Goal <degaullemugisha@gmail.com>
2023-10-19 12:16:27 -0700

    A script that creates a table second_table in the database hbtn_0c_0

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -7,4 +7,4 @@ CREATE TABLE IF NOT EXISTS second_table (
 
 -- Insert records into the second_table
 INSERT INTO second_table (id, name, score)
-VALUES (1, 'John' 10), (2, 'Alex', 3), (3, 'BOb', 14), (4, 'George', 8);
+VALUES (1, "John", 10), (2, "Alex", 3), (3, "BOb'" 14), (4, "George", 8);
