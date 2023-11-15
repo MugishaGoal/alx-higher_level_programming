3001d824aa87af5fc45d0d337018862bd1f6669a
Mugisha Goal <degaullemugisha@gmail.com>
2023-11-15 09:06:04 -0800

    script that prints a message depending of the number of arguments passed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-let argsCount = process.argv.length - 2;
+const argsCount = process.argv.length - 2;
 if (argsCount === 0) {
 	console.log('No argument');
-} else if (argsCount ===1) {
+} else if (argsCount == =1) {
 	console.log('Argument found');
 } else {
 	console.log('Arguments found');
