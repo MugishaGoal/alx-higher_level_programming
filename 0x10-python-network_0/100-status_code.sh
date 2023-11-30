2702450e1af034e1f33556623134d04ef43d5faf
Mugisha Goal <degaullemugisha@gmail.com>
2023-11-30 13:37:14 -0800

    A Bash script that sends a request to a URL passed as an argument

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # A Bash script that sends a request to a URL passed as an argument
-status_code=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-echo "$status_code"
+(echo -n $(curl -s -o /dev/null -w "%{http_code}" "$1"))
